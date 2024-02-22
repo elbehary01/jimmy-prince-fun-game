38621eea1ac4bdf837c33060c6569c3a8b84bc94
elbehary01 <29628969+elbehary01@users.noreply.github.com>
2024-02-22 08:25:30 +0100

    fix index.js error

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,16 @@
 // src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import './index.css';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const reactElement = document.getElementById('root');
+const root = ReactDOM.createRoot(reactElement);
+root.render(
   <Router>
     <App />
-  </Router>,
-  document.getElementById('root')
+  </Router>
 );
+
+reportWebVitals();
